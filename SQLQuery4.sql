7a2062c7dde1e228eee458006d15ea137797f8d8
dilayece <kzlkyadilayece@gmail.com>
2023-08-09 13:11:27 +0300

    Delete SQLQuery4.sql

--- SQLQuery4.sql
+++
@@ -1,7 +0,0 @@
-﻿insert into Customers values('Ensar Çelik',125,'05058728793','Mardin','ensarcelik@gmail.com')
-insert into Customers values('Damla Kaya',126,'05528728799','Van','damlakaya@gmail.com')
-insert into Customers values('Yagmur Türkmen',127,'05058728791','Gaziantep','yagmurturkmen@gmail.com')
-insert into Customers values('Kemal Bozkurt',128,'05059825574','İzmir','kemalbozkurt@gmail.com')
-insert into Customers values('Büşra Evşen',129,'05058728712','Elazığ','busraevsen@gmail.com')
-insert into Customers values('Fulya Tokluk',130,'05058951472','Van','fulyatokluk@gmail.com')
-
